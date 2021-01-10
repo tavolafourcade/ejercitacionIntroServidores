ad340f9f1c9463fa79b94c439ab2b041777bd48f
tavolafourcade <tavolafourcade@gmail.com>
2021-01-09 22:31:51 -0500

    Implemented a way to connect each HTML to their images and CSS using app.use

--- app.js
+++ app.js
@@ -12,3 +12,48 @@ app.get("/home", (req,res)=>{
 
   res.sendFile(htmlPath);
 });
+
+app.get("/babbage", (req,res)=>{
+  let htmlPath = path.resolve(__dirname, './views/babbage.html');
+
+  res.sendFile(htmlPath);
+});
+
+app.get("/berners-lee", (req,res)=>{
+  let htmlPath = path.resolve(__dirname, './views/berners-lee.html');
+
+  res.sendFile(htmlPath);
+});
+
+app.get("/clarke", (req,res)=>{
+  let htmlPath = path.resolve(__dirname, './views/clarke.html');
+
+  res.sendFile(htmlPath);
+});
+
+app.get("/hamilton", (req,res)=>{
+  let htmlPath = path.resolve(__dirname, './views/hamilton.html');
+
+  res.sendFile(htmlPath);
+});
+
+app.get("/hopper", (req,res)=>{
+  let htmlPath = path.resolve(__dirname, './views/hopper.html');
+
+  res.sendFile(htmlPath);
+});
+
+
+app.get("/lovelace", (req,res)=>{
+  let htmlPath = path.resolve(__dirname, './views/lovelace.html');
+
+  res.sendFile(htmlPath);
+});
+
+app.get("/turing", (req,res)=>{
+  let htmlPath = path.resolve(__dirname, './views/turing.html');
+
+  res.sendFile(htmlPath);
+});
+
+app.use(express.static('public'));
